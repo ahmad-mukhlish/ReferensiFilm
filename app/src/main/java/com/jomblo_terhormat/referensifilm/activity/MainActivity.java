23b679033c603cac66cb26eff012b2b6d8289d56
ahmad-mukhlish <ahmad_mukhlish_s@yahoo.co.id>
2017-09-16 10:44:24 +0700

    Edit the Main activiy

--- app/src/main/java/com/jomblo_terhormat/referensifilm/activity/MainActivity.java
+++ app/src/main/java/com/jomblo_terhormat/referensifilm/activity/MainActivity.java
@@ -1,16 +1,14 @@
 package com.jomblo_terhormat.referensifilm.activity;
 
 import android.app.LoaderManager;
-import android.content.Intent;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.jomblo_terhormat.referensifilm.R;
-import com.jomblo_terhormat.referensifilm.adapter.FilmListAdapter;
+import com.jomblo_terhormat.referensifilm.adapter.FilmRecycleViewAdapter;
 import com.jomblo_terhormat.referensifilm.entity.Film;
 import com.jomblo_terhormat.referensifilm.networking.FilmLoader;
 
@@ -20,7 +18,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Film>> {
 
     private static final int LOADER_ID = 54;
-    private FilmListAdapter filmListAdapter;
+    private FilmRecycleViewAdapter filmRecycleViewAdapter;
 
 
 
@@ -30,29 +28,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setContentView(R.layout.activity_main);
         LoaderManager loaderManager = getLoaderManager();
         loaderManager.initLoader(LOADER_ID, null, this);
-        ListView listView = (ListView) findViewById(R.id.list);
-        filmListAdapter = new FilmListAdapter(this,new ArrayList<Film>());
 
-//        FilmRecycleViewAdapter filmRecycleViewAdapter =
-//                new FilmRecycleViewAdapter(getContext(), films);
-//        RecyclerView recyclerView = rootView.findViewById(R.id.rvItems);
-//        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        filmRecycleViewAdapter = new FilmRecycleViewAdapter(this, new ArrayList<Film>());
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvItems);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 1);
 
-
-        listView.setAdapter(filmListAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Film clickedFilm = filmListAdapter.getItem(position) ;
-                Intent intent = new Intent(MainActivity.this,DetailActivity.class) ;
-                intent.putExtra("gambar",clickedFilm.getmBackdrop_path()) ;
-                intent.putExtra("judul",clickedFilm.getmTitle()) ;
-                intent.putExtra("deskripsi",clickedFilm.getmOverview()) ;
-                intent.putExtra("tanggal",clickedFilm.getmRelease_date()) ;
-                intent.putExtra("rating",clickedFilm.getmVote_average()) ;
-                startActivity(intent);
-            }
-        });
     }
 
 
@@ -63,12 +43,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<List<Film>> loader, List<Film> films) {
-        filmListAdapter.clear();
-        filmListAdapter.addAll(films);
+//        filmListAdapter.clear();
+//        filmListAdapter.addAll(films);
     }
 
     @Override
     public void onLoaderReset(Loader<List<Film>> loader) {
-        filmListAdapter.clear();
+//        filmListAdapter.clear();
     }
 }
